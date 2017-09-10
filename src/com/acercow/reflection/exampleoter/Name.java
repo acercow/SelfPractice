ac3323c4cdfd9ad8690e308d2236f013a92e9117
zhaosen <zhaosen@staff.weibo.com>
2017-09-11 00:25:23 +0800

    del duplicate

--- src/com/acercow/reflection/exampleoter/Name.java
+++
@@ -1,9 +0,0 @@
-package com.acercow.reflection.exampleoter;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.FIELD) @Documented
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Name {
-    String value() default "Gou Dan";
-}
