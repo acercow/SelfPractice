442bf195bfba9e18fb16421874e85655854d1f32
zhaosen <zhaosen@staff.weibo.com>
2017-11-10 11:14:21 +0800

    test

---
+++ src/com/acercow/async/JoinTest.java
@@ -0,0 +1,25 @@
+package com.acercow.async;
+
+public class JoinTest implements Runnable{
+      
+    public static int a = 0;  
+  
+    public void run() {  
+        for (int k = 0; k < 5; k++) {  
+            a = a + 1;  
+        }  
+    }
+
+    public static void main(String[] args) throws Exception {
+        Runnable r = new JoinTest();
+        Thread t = new Thread(r);
+        t.start();
+        //t.join(); //加入join()
+            /*
+             注意循环体内一定要有实际执行语句，否则编译器或JVM可能优化掉你的这段代码，视这段代
+             码为无效。
+            */
+        Thread.sleep(2000);
+        System.out.println(a);
+    }
+}  
--- src/com/acercow/collection/TestCollection.java
+++ src/com/acercow/collection/TestCollection.java
@@ -1,7 +1,15 @@
 package com.acercow.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestCollection {
     public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        for (int i : integers) {
+            System.out.println("integer : " + i);
+        }
         IterClass iterClass = new IterClass();
         for (String s : iterClass) {
 //            System.out.print(" -> " + s);
