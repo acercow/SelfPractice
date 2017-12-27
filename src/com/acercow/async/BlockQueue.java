dded2d7eb405fcf9800d431e9feabb0db14b0bdb
zhaosen <zhaosen@staff.weibo.com>
2017-12-27 19:27:25 +0800

    blockqueue

---
+++ src/com/acercow/async/BlockQueue.java
@@ -0,0 +1,32 @@
+package com.acercow.async;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BlockQueue  {
+    private List<Runnable> taskQueue = null;
+    private int limit;
+
+    public BlockQueue(int queueSize) {
+        taskQueue = new LinkedList<>();
+        this.limit = queueSize;
+    }
+
+
+    public void enqueue(Runnable runnable) throws InterruptedException {
+        synchronized (taskQueue) {
+            if (taskQueue.size() >= limit) {
+                taskQueue.wait();
+            }
+            taskQueue.add(runnable);
+        }
+    }
+
+    public Runnable dequeue() {
+        synchronized (taskQueue) {
+            if (taskQueue.size() <= 0) {
+
+            }
+        }
+    }
+}
